18500da07b70f2f6b9651b04b27c5ca40f6d8da0
RonexLemon <ronexondimu@gmail.com>
2023-11-28 18:06:08 +0300

    modify:contracts json

--- client/evoting/src/constants/addresses.js
+++ client/evoting/src/constants/addresses.js
@@ -1,2 +1,7 @@
-export const EvotingAddress ="0xBa3c3f1ca1fc0530cc4F12909D19CeDEB9d138CF"
+export const EvotingAddress = "0xa3f73177577Bfa8A2B1545916c4F25d4Bdc4fdc5" //"0xBa3c3f1ca1fc0530cc4F12909D19CeDEB9d138CF"
 export  const  id="7201f7bfa0132350c6ad444e2b53cee2" 
+
+
+// Deployer: 0xeC4C62d02Fb670F95Ab04416bC1D51831759BD4F
+// Deployed to: 0xa3f73177577Bfa8A2B1545916c4F25d4Bdc4fdc5
+// Transaction hash: 0xccaa7f956071b24c485dabea60e4d737f4f8c18af4f9706ca1b7964563d483b8
