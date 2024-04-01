c902d108cadda1f5bb49584465ff5c3ea225238c
kahiga254 <adamskahiga@gmail.com>
2024-04-01 19:05:52 +0300

    Update

--- backend/evoting/script/Counter.s.sol
+++ backend/evoting/script/Counter.s.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+/*import {Script, console2} from "forge-std/Script.sol";*/
 
-contract CounterScript is Script {
+contract CounterScript {
     function setUp() public {}
 
-    function run() public {
+   /* function run() public {
         vm.broadcast();
-    }
+    }*/
 }
